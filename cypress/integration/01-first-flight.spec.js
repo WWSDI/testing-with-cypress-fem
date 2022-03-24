3b9f32a1ac8a320686d0e1fb0c81bc553f895808
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-24 14:18:00 +1300

    add: the first test

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter');
+  });
+
+  it('should do something', () => {
+    cy.get('form').should('exist');
+  });
+
+  it("should have a label 'Item'", () => {
+    cy.contains('Item');
+  });
+  it("should have a label 'Filter'", () => {
+    cy.contains('Filter');
+  });
+  it('should put stuff in an input field', () => {
+    cy.get('[data-test="new-item-input"').type('Just do the work!');
+    cy.get('[data-test="add-item"').click();
+    cy.contains('Just do the work!');
+  });
+});
