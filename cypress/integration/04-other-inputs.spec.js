10fb195bbb155c37f884d82c66e9a0ee75e7cee0
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-25 09:37:14 +1300

    add: complete 04-other-inputs exercise

--- cypress/integration/04-other-inputs.spec.js
+++ cypress/integration/04-other-inputs.spec.js
@@ -8,9 +8,20 @@ describe('Secret Menu Items', () => {
     cy.get('#restaurant-visibility-filter').as('restaurant-filter');
   });
 
-  it('should set the range and verify it', () => {});
+  it('should set the range and verify it', () => {
+    cy.get('@rating-filter').invoke('val', 7).trigger('input')
+    cy.get('.name').should('have.length', 1)
+  });
 
-  it('should check the checkbox and verify it', () => {});
+  it('should check the checkbox and verify it', () => {
+    cy.get('input[type="checkbox"]').check().should('be.checked')
+  });
 
-  it('should select an option from the select and verify it', () => {});
+  it('should select an option from the select and verify it', () => {
+    cy.get('@restaurant-filter').select('KFC');
+    cy.get('@restaurant-filter').should('have.value', 'KFC');
+    cy.get('.whereToOrder')
+      .should('have.length', 7)
+      .each(($cell) => cy.wrap($cell).contains($cell.text()));
+  });
 });
