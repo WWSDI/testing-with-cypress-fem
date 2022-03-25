37b215e0e33d939fd518139a66bec22eddaa8247
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-25 16:29:53 +1300

    add: complete 06-secret-menu exercise

--- cypress/integration/06-secret-menu.spec.js
+++ cypress/integration/06-secret-menu.spec.js
@@ -39,4 +39,26 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  describe('should change menu items based on restaurant filter', () => {
+    for (let restaurant of restaurants) {
+      it(`when ${restaurant} is selected`, () => {
+        cy.get('#restaurant-visibility-filter').select(restaurant);
+        cy.get('#restaurant-visibility-filter').should('have.value', restaurant);
+        cy.get('.whereToOrder').each(($where) => cy.wrap($where).contains(restaurant));
+      });
+    }
+  });
+
+  describe('should change menu items based on rating filter', () => {
+    for (let rating of ratings) {
+      it(`when ${rating} is selected`, () => {
+        cy.get('#minimum-rating-visibility').invoke('val', rating).trigger('input');
+        cy.get('.popularity').each(($popularity) =>
+          // you don't always have to use Cypress chainers, sometimes sticking to jQuery and old fashion expect() is much simpler.
+          expect(+$popularity.text()).to.be.gte(rating)
+        );
+      });
+    }
+  });
 });
