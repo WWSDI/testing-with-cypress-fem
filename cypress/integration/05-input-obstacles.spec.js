8622ea9ecf096db446a85d66af9e599b1e3a783e
Shawn Su <ShawnsuDI@Shawns-MBP13-2018.local>
2022-03-25 15:33:24 +1300

    add: complete 05-input-obstacles exercise

--- cypress/integration/05-input-obstacles.spec.js
+++ cypress/integration/05-input-obstacles.spec.js
@@ -13,38 +13,43 @@ describe('Input obstacles', () => {
   });
 
   it('should control a select input', () => {
-    cy.get('[data-test="select-input"]');
-    cy.get('[data-test="select-result"]');
+    const selectedAvenger = 'Captain America';
+    cy.get('[data-test="select-input"]').select(selectedAvenger);
+    cy.get('[data-test="select-result"]').contains(selectedAvenger);
   });
 
   it('should find and control a checkbox input', () => {
-    cy.get('[data-test="checkbox-tomato"]');
-    cy.get('[data-test="checkbox-result"]').contains('(None)');
-    cy.get('[data-test="checkbox-result"]');
+    cy.get('[data-test="checkbox-tomato"]').click();
+    cy.get('[data-test="checkbox-result"]').contains('Tomato');
   });
 
   it('should find and control a radio input', () => {
-    cy.get('[data-test="radio-ringo"]');
-    cy.get('[data-test="radio-result"]');
+    cy.get('[data-test="radio-ringo"]').check();
+    cy.get('[data-test="radio-result"]').contains('Ringo');
   });
 
   it('should find and control a color input', () => {
-    cy.get('[data-test="color-input"]');
-    cy.get('[data-test="color-result"]');
+    const chosenColor = '#923f80';
+    // The strange thing here is that in Cypress docs I found an article claiming that triggering a 'change' event you can achieve the same effect, but it doesn't work here, possbily due to Cypress' updated API?
+    cy.get('[data-test="color-input"]').invoke('val', chosenColor).trigger('input');
+    cy.get('[data-test="color-result"]').contains(chosenColor);
   });
 
   it('should find and control a date input', () => {
-    cy.get('[data-test="date-input"]');
-    cy.get('[data-test="date-result"]');
+    const chosenDate = '2022-03-25'; // has to be in the ISO formate: yyyy-mm-dd, otherwise wouldn't work
+    // if you use .type() then you don't have to trigger an event
+    cy.get('[data-test="date-input"]').type(chosenDate)
+    // cy.get('[data-test="date-input"]').invoke('val', chosenDate).trigger('input');
+    cy.get('[data-test="date-result"]').contains(chosenDate);
   });
 
   it('should find and control a range input', () => {
-    cy.get('[data-test="range-input"]');
-    cy.get('[data-test="range-result"]');
+    cy.get('[data-test="range-input"]').invoke('val', 10).trigger('input')
+    cy.get('[data-test="range-result"]').contains(10);
   });
 
   it('should find and control a file input', () => {
-    cy.get('[data-test="file-input"]');
+    cy.get('[data-test="file-input"]')
     cy.get('[data-test="file-result"]');
   });
 });
